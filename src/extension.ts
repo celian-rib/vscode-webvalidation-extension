a3319fc220da0f6320d0cad92f3a800a88ccce38
celian <celian.riboulet@gmail.com>
2021-02-17 17:08:46 +0100

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { Interface } from 'readline';
 import * as vscode from 'vscode';
 const axios = require('axios').default;
 
